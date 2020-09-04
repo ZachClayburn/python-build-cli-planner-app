d454c7643eb7ee42d459732e0fc367b99bfbfec2
Zach Clayburn <zachclayburn@gmail.com>
2020-09-04 12:22:26 -0600

    Task 4

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,8 +1,14 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
     @abstractmethod
     def is_due(self):
-        raise NotADirectoryError
+        raise NotImplementedError
+
+
+class DeadlinedReminder(Iterable, ABC):
+    @abstractmethod
+    def is_due(self):
+        raise NotImplementedError
